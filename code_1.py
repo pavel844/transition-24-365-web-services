07d56d1dcc7c42619a0d65fcb58eff0929730e70
Pavel <143156330+pavel844@users.noreply.github.com>
2023-11-01 00:43:30 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+}
+    });
+}
+    const data = [];
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+main();
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    return data;
+
+function main() {
+    }
+    data.forEach(item => {
+
+        console.log("Random Number: " + item);
+    const data = [];
+    return data;
+main();
+    });
+
+    }
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+
+
+}
+function generateRandomData() {
+function main() {
+}
